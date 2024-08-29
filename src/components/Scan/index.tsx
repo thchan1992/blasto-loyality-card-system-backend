d0405621a04a354a338e610101a5f356387e6ed1
Chan <chan@iPod-Classic-128GB.local>
2024-08-29 15:51:26 +0100

    added qr camera

--- src/components/Scan/index.tsx
+++ src/components/Scan/index.tsx
@@ -7,24 +7,39 @@ import { Business } from "@/lib/types/Business";
 import { Bubblegum_Sans } from "next/font/google";
 import React, { useEffect, useState } from "react";
 
-import { IDetectedBarcode, Scanner } from "@yudiel/react-qr-scanner";
+import { Scanner } from "@yudiel/react-qr-scanner";
 export const Scan = () => {
+  const [showCamera, setShowCamera] = useState<boolean>(false);
   const handleDecode = (result) => {
-    console.log("QR Code Result:", result.rawValue);
+    if (result && result.length > 0) {
+      const rawValue = result[0].rawValue;
+      console.log("QR Code Raw Value:", rawValue);
+      setShowCamera(false);
+    }
   };
 
   const handleError = (error) => {
-    console.error("QR Scanner Error:", error);
+    console.error("QR Scanner Error:1", error);
   };
 
   return (
     <div className="flex items-center justify-center">
-      <div className="h-1/2 w-full pl-2 pr-2">
-        <Scanner
-          constraints={{ facingMode: "environment" }}
-          onScan={handleDecode}
-          onError={handleError}
-        />
+      <div className="flex h-1/2 w-full flex-col pl-2 pr-2">
+        {showCamera && (
+          <Scanner
+            constraints={{ facingMode: "environment" }}
+            onScan={(result) => handleDecode(result)}
+            onError={handleError}
+          />
+        )}
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            showCamera ? setShowCamera(false) : setShowCamera(true);
+          }}
+        >
+          {showCamera ? "Close Camera" : "Open Camera"}
+        </button>
       </div>
     </div>
   );

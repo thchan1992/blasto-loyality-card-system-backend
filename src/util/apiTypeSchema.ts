491f85a7c561a80d9f3e8200abeda234aa1789c3
Chan <chan@iPod-Classic-128GB.local>
2024-09-05 08:13:09 +0100

    set up zod

---
+++ src/util/apiTypeSchema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const businessUpdateSchema = z.object({
+  name: z.string(),
+  logo: z.string(),
+  loyaltyProgram: z.union([z.literal(5), z.literal(10)]),
+  clerkUserId: z.string(),
+});

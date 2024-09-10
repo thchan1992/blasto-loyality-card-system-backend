4734d14c8d993c145e220ed3994417b2f8d374e1
Chan <chan@ipodclasic128gb.home>
2024-09-10 23:14:25 +0200

    added hero section

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -5,20 +5,47 @@ const Hero = () => {
     <>
       <section
         id="home"
-        className="relative z-10 overflow-hidden bg-white pb-16 pt-[120px] dark:bg-gray-dark md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
+        // className="relative z-10 overflow-hidden bg-white pb-16 pt-[120px] dark:bg-gray-dark md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
       >
-        <div className="container">
+        <div
+          className="hero min-h-screen"
+          style={{
+            backgroundImage: "url(images/hero/hero.jpg)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">
+                {" "}
+                Blasta - Lightweight Digital Stamp System
+              </h1>
+              <p className="mb-5">
+                Get started in minutes with our simple, no-fuss digital stamp
+                system. Scan customer QR codes directly from our website, reward
+                them instantly, and boost loyalty with minimal effort. No
+                complex integration—just an easy, streamlined solution!
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+        {/* <div className="container">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  Free and Open-Source Next.js Template for Startup & SaaS
+                  Blasta - Lightweight Digital Stamp System: Easy Setup, Scan QR
+                  Codes, Reward Customers.
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                  Startup is free Next.js template for startups and SaaS
-                  business websites comes with all the essential pages,
-                  components, and sections you need to launch a complete
-                  business website, built-with Next 13.x and Tailwind CSS.
+                  Get started in minutes with our simple, no-fuss digital stamp
+                  system. Scan customer QR codes directly from our website,
+                  reward them instantly, and boost loyalty with minimal effort.
+                  No complex integration—just an easy, streamlined solution!
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
@@ -37,7 +64,7 @@ const Hero = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
         <div className="absolute right-0 top-0 z-[-1] opacity-30 lg:opacity-100">
           <svg
             width="450"

1cfe6592c1dea5f9e16ef0e927feae63e380a300
Chan <chan@iPod-Classic-128GB.local>
2024-09-13 12:21:06 +0100

    adjusted style on dashboard
    d

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -8,6 +8,7 @@ import { Bubblegum_Sans } from "next/font/google";
 import React, { useEffect, useState } from "react";
 import { UploadForm } from "./S3UploadForm";
 import Modal from "../Modal";
+import { Stats } from "./Stats";
 export const Profile = () => {
   const { handleApiErrors } = useHandleApiErrors();
 
@@ -99,62 +100,86 @@ export const Profile = () => {
   };
 
   return (
-    <div className="flex items-center justify-center">
-      {!isLoading && (
-        <div>
-          <div className="card card-compact m-4 w-96 bg-base-100 shadow-xl">
-            <figure>
-              <img src={business.logo} alt="Logo" />
-            </figure>
-            <div className="card-body">
-              {business.email}
-              <h2 className="card-title">Profile</h2>
-              Total Stamps: {totalStamps}
-              <p className="flex flex-col items-center justify-center border-2 p-1">
-                <UploadForm
-                  onFileUrlChange={handleLogoChange}
-                  oldFileUrl={business.logo}
-                />
-                <input
-                  value={business.name}
-                  type="text"
-                  placeholder="Business name"
-                  className="input input-bordered w-full max-w-xs"
-                  onChange={handleNameChange}
-                />
-                <label className="swap swap-flip pt-2 text-9xl">
-                  {/* Bind checkbox checked state to the loyalty program value */}
-                  <input
-                    type="checkbox"
-                    checked={business.loyaltyProgram !== 5}
-                    // onChange={handleLoyaltyProgramChange}
-                    onChange={handleLoyaltyProgramChange}
+    <div className="flex items-center justify-center border-red-100 ">
+      <div className="container">
+        <div className="-mx-4 flex flex-wrap">
+          <div className="w-full px-4 lg:w-7/12 xl:w-8/12 ">
+            <div className="flex flex-row justify-between ">
+              {!isLoading && (
+                <div className="card card-compact m-4 mb-10 mt-20 w-96 bg-base-100 shadow-xl lg:w-full xl:w-2/3">
+                  <figure>
+                    <img
+                      src={business.logo}
+                      alt="Logo"
+                      className="h-88 w-88 bg-white object-cover"
+                    />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title"> {business.email}</h2>
+                    {/* Total Stamps: {totalStamps}
+                    Rewards Redeemed: {business.rewardsRedeemed} */}
+                    <Stats
+                      data={[
+                        { title: "Total Stamps", value: totalStamps, desc: "" },
+                        {
+                          title: "Rewards Redeemed",
+                          value: business.rewardsRedeemed,
+                          desc: "",
+                        },
+                      ]}
+                    />
+
+                    <div className="glass rounded-box ">
+                      <p className="flex flex-col justify-center p-1 ">
+                        <p className="pl-10 pt-2">Change your business logo</p>
+                        <UploadForm
+                          onFileUrlChange={handleLogoChange}
+                          oldFileUrl={business.logo}
+                        />
+                        <input
+                          value={business.name}
+                          type="text"
+                          placeholder="Business name"
+                          className="input input-bordered  w-full"
+                          onChange={handleNameChange}
+                        />
+                        <label className="swap swap-flip pt-2 text-9xl">
+                          {/* Bind checkbox checked state to the loyalty program value */}
+                          <input
+                            type="checkbox"
+                            checked={business.loyaltyProgram !== 5}
+                            // onChange={handleLoyaltyProgramChange}
+                            onChange={handleLoyaltyProgramChange}
+                          />
+                          {/* Display "10" when checkbox is checked, "5" when unchecked */}
+                          <div className="swap-on">🔟</div>
+                          <div className="swap-off">5️⃣</div>
+                        </label>
+                      </p>
+                    </div>
+                    <div className="card-actions justify-end">
+                      <button
+                        className="btn btn-primary"
+                        onClick={handleChangeBusiness}
+                      >
+                        Change Profile
+                      </button>
+                    </div>
+                  </div>
+
+                  <Modal
+                    message={warningMessage}
+                    visible={showWarning}
+                    onConfirm={() => {
+                      setShowWarning(false);
+                    }}
                   />
-                  {/* Display "10" when checkbox is checked, "5" when unchecked */}
-                  <div className="swap-on">🔟</div>
-                  <div className="swap-off">5️⃣</div>
-                </label>
-                Rewards Redeemed: {business.rewardsRedeemed}
-              </p>
-              <div className="card-actions justify-end">
-                <button
-                  className="btn btn-primary"
-                  onClick={handleChangeBusiness}
-                >
-                  Change Profile
-                </button>
-              </div>
+                </div>
+              )}
             </div>
           </div>
-          <Modal
-            message={warningMessage}
-            visible={showWarning}
-            onConfirm={() => {
-              setShowWarning(false);
-            }}
-          />
         </div>
-      )}
+      </div>
     </div>
   );
 };

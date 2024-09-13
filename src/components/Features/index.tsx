2b91f99b502e96589cae654e03b4b805b4b4e1df
Chan <chan@iPod-Classic-128GB.local>
2024-09-13 22:42:13 +0100

    added styling to main page

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import SectionTitle from "../Common/SectionTitle";
 import SingleFeature from "./SingleFeature";
 import featuresData from "./featuresData";
@@ -18,9 +19,53 @@ const Features = () => {
               <SingleFeature key={feature.id} feature={feature} />
             ))}
           </div>
-          <div className="flex items-center justify-center">
+          <div className="mt-20 grid grid-cols-1 items-center justify-center gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-2">
+            <div>
+              <SectionTitle
+                title="Ask your customers to try our Blasta App"
+                paragraph="Available on both Platform"
+                center
+              />
+              <div className="flex flex-row justify-evenly">
+                <Image
+                  src="/images/features/app-store.svg"
+                  alt="about image"
+                  width={180}
+                  height={180}
+                  className=""
+                />
+                <Image
+                  src="/images/features/play-store.svg"
+                  alt="about image"
+                  width={180}
+                  height={180}
+                  className=""
+                />
+              </div>
+            </div>
             <div className="carousel mt-10 rounded-box border-2 border-white">
               <div className="carousel-item">
+                {/* <div className="carousel-item">
+                  <img
+                    src="images/features/dashboard.png"
+                    alt="Burger"
+                    className="h-128 w-64 bg-white object-cover"
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="images/features/dashboard2.png"
+                    alt="Burger"
+                    className="h-128 w-64 bg-white object-cover"
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="images/features/scanner.png"
+                    alt="Burger"
+                    className="h-128 w-64 bg-white object-cover"
+                  />
+                </div> */}
                 <img
                   src="images/features/landing.png"
                   alt="Burger"
@@ -40,27 +85,6 @@ const Features = () => {
                   alt="Burger"
                   className="h-128 w-64 bg-white object-cover"
                 />
-              </div>
-              <div className="carousel-item">
-                <img
-                  src="images/features/shop-list.png"
-                  alt="Burger"
-                  className="h-128 w-64 bg-white object-cover"
-                />
-              </div>
-              <div className="carousel-item">
-                <img
-                  src="images/features/shop-list.png"
-                  alt="Burger"
-                  className="h-128 w-64 bg-white object-cover"
-                />
-                <div className="carousel-item">
-                  <img
-                    src="images/features/shop-list.png"
-                    alt="Burger"
-                    className="h-128 w-64 bg-white object-cover"
-                  />
-                </div>
               </div>
             </div>
           </div>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -58,20 +58,7 @@ const Header = () => {
                   sticky ? "py-5 lg:py-2" : "py-8"
                 } `}
               >
-                <Image
-                  src="/images/logo/logo-2.svg"
-                  alt="logo"
-                  width={140}
-                  height={30}
-                  className="w-full dark:hidden"
-                />
-                <Image
-                  src="/images/logo/logo.svg"
-                  alt="logo"
-                  width={140}
-                  height={30}
-                  className="hidden w-full dark:block"
-                />
+                <p className="font-extrabold">BLASTA</p>
               </Link>
             </div>
             <div className="flex w-full items-center justify-between px-4">
